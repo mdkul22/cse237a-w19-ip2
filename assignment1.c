6d4655a2d1267421bfb5713d00a368c9fcc9dfc1
mdkul22 <mdkul22@gmail.com>
2019-02-07 02:03:19 -0800

    First checkpoint code check 1

--- assignment1.c
+++ assignment1.c
@@ -7,32 +7,141 @@
 void init_shared_variable(SharedVariable* sv) {
 	// You can initialize the shared variable if needed.
 	sv->bProgramExit = 0;
+	sv->small_mic = 0;
+	sv->big_mic = 0;
+	sv->touch = 0;
 }
 
 void init_sensors(SharedVariable* sv) {
+	pinMode(PIN_BUTTON, INPUT);
+	pinMode(PIN_BIG, INPUT);
+	pinMode(PIN_SMALL, INPUT);
+	// using metal touch sensor which acts the same way
+	pinMode(PIN_TOUCH, INPUT);
+	softPwmCreate(PIN_DIP_BLU, 0, 0xFF);
+	softPwmCreate(PIN_DIP_RED, 0, 0xFF);
+	softPwmCreate(PIN_DIP_GRN, 0, 0xFF);
+	softPwmCreate(PIN_SMD_BLU, 0, 0xFF);
+	softPwmCreate(PIN_SMD_RED, 0, 0xFF);
+	softPwmCreate(PIN_SMD_GRN, 0, 0xFF);
+	pinMode(PIN_ALED, OUTPUT);
+	pinMode(PIN_BUZZER, OUTPUT);
 }
 
 void body_button(SharedVariable* sv) {
-	sv->bProgramExit = 1;
+	sv->bProgramExit = (sv->bProgramExit+1)%2;
 }
 
 void body_threecolor(SharedVariable* sv) {
+	if(sv->bProgramExit){
+		softPwmWrite(PIN_DIP_BLU, 0);
+		softPwmWrite(PIN_DIP_RED, 0);
+		softPwmWrite(PIN_DIP_GRN, 0);
+	}
+	else{
+		if(sv->small_mic){
+			softPwmWrite(PIN_DIP_BLU, 0x00);
+			softPwmWrite(PIN_DIP_RED, 0xFF);
+		  softPwmWrite(PIN_DIP_GRN, 0x00);
+		}
+		else{
+			softPwmWrite(PIN_DIP_BLU, 0xFF);
+			softPwmWrite(PIN_DIP_RED, 0x00);
+		  softPwmWrite(PIN_DIP_GRN, 0x00);
+		}
+	}
 }
 
 void body_big(SharedVariable* sv) {
+	int val = digitalRead(PIN_BIG);
+	if(val){
+		sv->big_mic = 1;
+		delay(500);
+	}
+	else{
+		sv->big_mic = 0;
+		delay(500);
+	}
 }
 
 void body_small(SharedVariable* sv) {
+	int val = digitalRead(PIN_SMALL);
+	if(val){
+		sv->small_mic = 1;
+		delay(500);
+	}
+	else{
+		sv->small_mic = 0;
+		delay(500);
+	}
 }
 
 void body_touch(SharedVariable* sv) {
+	int val = digitalRead(PIN_TOUCH);
+	if(val){
+		sv->touch = 1;
+		delay(500);
+	}
+	else{
+		sv->touch = 0;
+		delay(500);
+	}
 }
 
 void body_rgbcolor(SharedVariable* sv) {
+	if(sv->bProgramExit){
+		softPwmWrite(PIN_SMD_BLU, 0);
+		softPwmWrite(PIN_SMD_RED, 0);
+		softPwmWrite(PIN_SMD_GRN, 0);
+	}
+	else{
+		if(sv->small_mic==0&&sv->touch==0){
+			softPwmWrite(PIN_DIP_BLU, 0x00);
+			softPwmWrite(PIN_DIP_RED, 0xFF);
+			softPwmWrite(PIN_DIP_GRN, 0x00);
+		}
+		else if(sv->small_mic==1&&sv->touch==0){
+			softPwmWrite(PIN_DIP_BLU, 0xC8);
+			softPwmWrite(PIN_DIP_RED, 0xEE);
+			softPwmWrite(PIN_DIP_GRN, 0x00);
+		}
+		else if(sv->small_mic==0&&sv->touch==1){
+			softPwmWrite(PIN_DIP_BLU, 0x00);
+			softPwmWrite(PIN_DIP_RED, 0x80);
+			softPwmWrite(PIN_DIP_GRN, 0xFF);
+		}
+		else{
+			softPwmWrite(PIN_DIP_BLU, 0xFF);
+			softPwmWrite(PIN_DIP_RED, 0x00);
+			softPwmWrite(PIN_DIP_GRN, 0xFF);
+		}
+	}
 }
 
 void body_aled(SharedVariable* sv) {
+	if(sv->bProgramExit)
+	{
+		digitalWrite(PIN_ALED, LOW);
+	}
+	else{
+		digitalWrite(PIN_ALED, HIGH);
+		printf("LED Active");
+		delay(1000);
+	}
 }
 
 void body_buzzer(SharedVariable* sv) {
+	if(sv->bProgramExit){
+		softPwmWrite(PIN_DIP_BLU, 0);
+		softPwmWrite(PIN_DIP_RED, 0);
+		softPwmWrite(PIN_DIP_GRN, 0);
+	}
+	else{
+		if(sv->big_mic){
+			digitalWrite(PIN_BUZZER, HIGH);
+		}
+		else{
+			digitalWrite(PIN_BUZZER, LOW);
+		}
+	}
 }
--- assignment1.h
+++ assignment1.h
@@ -7,32 +7,24 @@
 //
 // 1. Button
 #define PIN_BUTTON 0
-
 // 2. Big audio sensor
 #define PIN_BIG 4
-
 // 3. Small audio sensor
 #define PIN_SMALL 5
-
 // 4. Touch Sensor
 #define PIN_TOUCH 6
-
 // 5. DIP RGB LED (Dual In-line Package)
 #define PIN_DIP_RED 7
 #define PIN_DIP_GRN 8
 #define PIN_DIP_BLU 9
-
 // 6. SMD RGB LED (Surface Mount Device)
 #define PIN_SMD_RED 27
 #define PIN_SMD_GRN 28
 #define PIN_SMD_BLU 29
-
 // 7. Auto-flash Red
 #define PIN_ALED 12
-
 // 8. Buzzer
 #define PIN_BUZZER 13
-
 // B. Shared structure
 // All thread fuctions get a shared variable of the structure
 // as the function parameter.

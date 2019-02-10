93e5c995ad4a596b94461c7b6b3ce3030084dcb3
mdkul22 <mdkul22@gmail.com>
2019-02-10 14:06:33 -0800

    workload added

---
+++ section2/workload/2/deadlines.c
@@ -0,0 +1,12 @@
+// Generated deadlines for the test workload
+// This file has to be included only in main_week4.c
+long long workloadDeadlines[] = {
+	200000,
+	600000,
+	400000,
+	600000,
+	300000,
+	600000,
+	400000,
+	150000,
+};

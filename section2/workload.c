ca5259c2a2d53c891ac8620f5e6a9eb257f7aba6
mdkul22 <mdkul22@gmail.com>
2019-02-15 12:46:58 -0800

    not present

--- section2/workload.c
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /classes/wi19/cse237A-a/project/part2/section2/workload.c was not found on this server.</p>
-<hr>
-<address>Apache/2.4.29 (Ubuntu) Server at cseweb.ucsd.edu Port 80</address>
-</body></html>

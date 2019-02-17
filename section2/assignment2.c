ba0c27d8aa5af0c14f00e85e93977bbaad1c6425
mdkul22 <mdkul22@gmail.com>
2019-02-17 09:34:03 +0000

    x

--- section2/assignment2.c
+++ section2/assignment2.c
@@ -85,13 +85,13 @@ void learn_workloads(SharedVariable* sv) {
 		if(util>1)
 		printf("Its beyond 100%\n");
 		sv->tasks[i] = i;
-		printf("%d\t", sv->tasks[j]);
+		printf("%d\t", sv->tasks[i]);
 	}
 	int temp;
 	for(int j = 0; j < 8; j++){
 		for(int p = j+1; p < 8; p++)
 		{
-			if(workloadDeadlines[p] < workloadDeadlines[j])
+			if(workloadDeadlines[p]-sv->duration[p] < workloadDeadlines[j]-sv->duration[j])
 			{
 				temp = sv->tasks[j];
 				sv->tasks[j] = sv->tasks[p];
